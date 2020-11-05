e352ee0c90a60c3a3ffc1f7d5d0635d613e51fe2
Andrey Avtushenko <palaxius@mail.ru>
2020-11-05 13:57:49 +0300

    hooked up materialize-css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-  <template>
+<template>
   <div id="app">
     <router-view />
-    <h1>sdfs</h1>
   </div>
-  </template>
+</template>
 
-  <style lang="scss">
-  @import "~materialize-css/dist/css/materialize.min.css";
-  </style>
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
